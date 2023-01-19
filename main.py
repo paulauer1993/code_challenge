4787a933c08e8da5461ddf8f1a1a26bc74e2562f
Paul Auer <paul.auer@visma.com>
2023-01-19 15:14:29 +0200

    Add parse function

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import argparse
 
 from math import sqrt
 from urllib.request import urlopen
+
+
+def parse_command():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("x", type=float)
+    parser.add_argument("y", type=float)
+    parser.add_argument("filename")
+
+    args = parser.parse_args()
+    return args.x, args.y, args.filename
